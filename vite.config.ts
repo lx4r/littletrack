861a1a0fead55b536e2014511af513f3ce9ef6e2
lx4r <lx4r@users.noreply.github.com>
2023-11-24 13:28:10 +0100

    Add and activate `vite-plugin-pwa`

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   test: {
     environment: "happy-dom",
     setupFiles: ["./vitest.setup.ts"],
     unstubGlobals: true,
     globals: true,
+  },
+  // Use different ports than Vite's default ports to avoid this app's service worker influencing other apps.
+  server: {
+    port: 5174,
+  },
+  preview: {
+    port: 4174,
   },
 });
